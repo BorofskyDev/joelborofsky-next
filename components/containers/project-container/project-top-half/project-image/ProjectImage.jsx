fafef1b75aa6814542833e543ae49b1cec948f31
solovyovsdom <joelborofsky@mac.com>
2024-11-11 21:20:53 -0600

    Refactor ProjectTopHalf styles for laptop responsiveness; add ProjectLinks styles and implement multiple ProjectContainer instances in ProjectsSection

--- components/containers/project-container/project-top-half/project-image/ProjectImage.jsx
+++ components/containers/project-container/project-top-half/project-image/ProjectImage.jsx
@@ -4,7 +4,7 @@ import styles from './ProjectImage.module.scss'
 export default function ProjectImage({ src, alt }) {
   return (
     <div className={styles.projectImage}>
-      <Image className={`bg-blue ${styles.projectImage}`} src={src} alt={alt} width={1260} height={1080}  />
+      <Image className='bg-blue' src={src} alt={alt} width={1260} height={1080}  />
     </div>
   )
 }
--- components/pages/splash/sections/projects/ProjectsSection.jsx
+++ components/pages/splash/sections/projects/ProjectsSection.jsx
@@ -12,6 +12,8 @@ export default function ProjectsSection() {
   return (
     <section id='projects' className={styles.projectsSection}>
       <SectionHeader className='bg-red'>Projects</SectionHeader>
+    <div className={styles.projectsContainer}>
+
     <ProjectContainer
     className='bg-vibrant-blue'
     title='This Site!'
@@ -34,6 +36,94 @@ export default function ProjectsSection() {
     iconFill='fill-vibrant-blue'
     />
    
+   <ProjectContainer
+    className='bg-vibrant-red'
+    title='Rebekah Fowler Profile'
+    alt='Rebekah Fowler Profile'
+    src={rfProjectImg}
+    blogHref='https://rebekahfowler.com/'
+    liveSiteHref='https://rebekahfowler.com/'
+    techIcon1={icons.nextjs.path}
+    xmlns1={icons.nextjs.xmlns}
+    viewBox1={icons.nextjs.viewBox}
+    techIcon2={icons.sass.path}
+    xmlns2={icons.sass.xmlns}
+    viewBox2={icons.sass.viewBox}
+    techIcon3={icons.framerMotion.path}
+    xmlns3={icons.framerMotion.xmlns}
+    viewBox3={icons.framerMotion.viewBox}
+    decorativeIcon={icons.profile.path}
+    xmlns4={icons.profile.xmlns}
+    viewBox4={icons.profile.viewBox}
+    iconFill='fill-vibrant-red'
+   />
+   
+   <ProjectContainer
+    className='bg-vibrant-green'
+    title='NAWA! '
+    alt='NAWA intro page with login'
+    src={nawaProjectImg}
+    blogHref='/'
+    liveSiteHref='/'
+    techIcon1={icons.nextjs.path}
+    xmlns1={icons.nextjs.xmlns}
+    viewBox1={icons.nextjs.viewBox}
+    techIcon2={icons.sass.path}
+    xmlns2={icons.sass.xmlns}
+    viewBox2={icons.sass.viewBox}
+    techIcon3={icons.firebase.path}
+    xmlns3={icons.firebase.xmlns}
+    viewBox3={icons.firebase.viewBox}
+    decorativeIcon={icons.work.path}
+    xmlns4={icons.work.xmlns}
+    viewBox4={icons.work.viewBox}
+    iconFill='fill-vibrant-green'
+   />
+   
+   <ProjectContainer
+    className='bg-vibrant-pink'
+    title='Moments to Memories'
+    alt='moments to memories splash page'
+    src={momentsToMemoriesProjectImg}
+    blogHref='/'
+    liveSiteHref='/'
+    techIcon1={icons.nextjs.path}
+    xmlns1={icons.nextjs.xmlns}
+    viewBox1={icons.nextjs.viewBox}
+    techIcon2={icons.sass.path}
+    xmlns2={icons.sass.xmlns}
+    viewBox2={icons.sass.viewBox}
+    techIcon3={icons.firebase.path}
+    xmlns3={icons.firebase.xmlns}
+    viewBox3={icons.firebase.viewBox}
+    decorativeIcon={icons.camera.path}
+    xmlns4={icons.camera.xmlns}
+    viewBox4={icons.camera.viewBox}
+    iconFill='fill-vibrant-pink'
+   />
+   
+   <ProjectContainer
+    className='bg-orange'
+    title='Criminal Rolls'
+    alt='criminal rolls splash page'
+    src={criminalRollsProjectImg}
+    blogHref='/'
+    liveSiteHref='/'
+    techIcon1={icons.nextjs.path}
+    xmlns1={icons.nextjs.xmlns}
+    viewBox1={icons.nextjs.viewBox}
+    techIcon2={icons.tailwind.path}
+    xmlns2={icons.tailwind.xmlns}
+    viewBox2={icons.tailwind.viewBox}
+    techIcon3={icons.firebase.path}
+    xmlns3={icons.firebase.xmlns}
+    viewBox3={icons.firebase.viewBox}
+    decorativeIcon={icons.cinnamonRoll.path}
+    xmlns4={icons.cinnamonRoll.xmlns}
+    viewBox4={icons.cinnamonRoll.viewBox}
+    iconFill='fill-orange'
+   />
+    </div>
     </section>
   )
 }
