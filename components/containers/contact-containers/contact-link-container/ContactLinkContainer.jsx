5444d137db1e609045da2e5722aedbb586b7ba63
solovyovsdom <joelborofsky@mac.com>
2024-11-13 21:24:12 -0600

    Add ContactLinkContainer and update ContactSection; implement layout and styling for contact links

--- components/containers/contact-containers/contact-link-container/ContactLinkContainer.jsx
+++ components/containers/contact-containers/contact-link-container/ContactLinkContainer.jsx
@@ -1,33 +1,52 @@
 import ExternalButtonLink from '@/components/links/external-button-link/ExternalButtonLink'
-import styles from './ContactLinkContainer.scss'
+import InternalLinkBox from '@/components/links/internal-link-box/InternalLinkBox'
+import LargeTitleContainer from '../../title-containers/large-title-container/LargeTitleContainer'
+import MediumIcon from '@/components/icons/medium-icon/MediumIcon'
+import { icons } from '@/components/icons/icons'
+import styles from './ContactLinkContainer.module.scss'
 
 export default function ContactLinkContainer() {
   return (
+    <>
+    <LargeTitleContainer className='relative bg-blue'>
+        <MediumIcon
+            icon={icons.world.path}
+            viewBox={icons.world.viewBox}
+            xmlns={icons.world.xmlns}
+            className={`bg-green fill-vibrant-blue ${styles.icon}`}
+        />
+        Find me on the world wide web!</LargeTitleContainer>
     <ul className={styles.contactLinkContainer}>
-      <li>
+      
         <ExternalButtonLink
           className='bg-red'
           href='https://github.com/BorofskyDev'
         >
           My GitHub
         </ExternalButtonLink>
-      </li>
-      <li>
+      
         <ExternalButtonLink
           className='bg-yellow'
           href='https://codepen.io/borofskyDev'
         >
           My CodePen
         </ExternalButtonLink>
-      </li>
-      <li>
+      
         <ExternalButtonLink
           className='bg-pink'
           href='https://www.linkedin.com/in/joelborofsky/'
         >
-          My CodePen
+          My LinkedIn
         </ExternalButtonLink>
-      </li>
+      
+        <InternalLinkBox
+         
+          href='/contact'
+        >
+          Use my contact form!
+        </InternalLinkBox>
+      
     </ul>
+    </>
   )
 }
--- components/pages/splash/sections/contact/ContactSection.jsx
+++ components/pages/splash/sections/contact/ContactSection.jsx
@@ -2,28 +2,54 @@ import SectionHeader from '@/components/typography/headers/section-header/Sectio
 import PageHeader from '@/components/typography/headers/page-header/PageHeader'
 import LargeTitleContainer from '@/components/containers/title-containers/large-title-container/LargeTitleContainer'
 import { icons } from '@/components/icons/icons'
-import styles from './ContactSection.module.scss'
 import MediumIcon from '@/components/icons/medium-icon/MediumIcon'
 import TextContainer from '@/components/containers/text-container/TextContainer'
-
+import ImageContainer from '@/components/containers/image-container/ImageContainer'
+import ContactLinkContainer from '@/components/containers/contact-containers/contact-link-container/ContactLinkContainer'
+import contactImage from '@/public/img/profile/joel-hat-cutout.png'
+import styles from './ContactSection.module.scss'
 export default function ContactSection() {
   return (
     <section className={`bg-violet ${styles.contactSection}`}>
-      <SectionHeader className='bg-green'>Contact</SectionHeader>
-      <PageHeader>Hello Again!</PageHeader>
-      <LargeTitleContainer className='bg-yellow'>
-        <span>Let's work together!</span>
-        <MediumIcon
-          icon={icons.handshake.path}
-          viewBox={icons.handshake.viewBox}
-          xmlns={icons.handshake.xmlns}
-          className='bg-vibrant-purple fill-blue'
+      <div className={styles.containerOne}>
+        <SectionHeader className='bg-green'>Contact</SectionHeader>
+        <PageHeader>Hello Again!</PageHeader>
+        <LargeTitleContainer className='relative bg-yellow'>
+          <span>Let's work together!</span>
+          <MediumIcon
+            icon={icons.handshake.path}
+            viewBox={icons.handshake.viewBox}
+            xmlns={icons.handshake.xmlns}
+            className={`bg-vibrant-purple fill-blue ${styles.icon}`}
+          />
+        </LargeTitleContainer>
+        <TextContainer>
+          I&apos;m open for consulting, freelance work, or employment. Feel free
+          to reach out!
+        </TextContainer>
+      </div>
+      <div className={styles.containerTwo}>
+        <ImageContainer
+          src={contactImage}
+          alt='Joel Borofsky'
+          className='bg-vibrant-blue'
+          imageBg='bg-vibrant-orange'
+          titleContainerBg='bg-vibrant-pink'
+          title='Joel Borofsky'
+          note='so slavic'
+          iconBg1='bg-vibrant-red'
+          iconFill1='fill-vibrant-blue'
+          icon1={icons.fireHeart.path}
+          xmlns1={icons.fireHeart.xmlns}
+          viewbox1={icons.fireHeart.viewBox}
+          iconBg2='bg-blue'
+          iconFill2='fill-vibrant-red'
+          icon2={icons.cuteCat.path}
+          xmlns2={icons.cuteCat.xmlns}
+          viewBox2={icons.cuteCat.viewBox}
         />
-      </LargeTitleContainer>
-      <TextContainer>
-        I&apos;m open for consulting, freelance work, or employment. Feel free
-        to reach out!
-      </TextContainer>
+        <ContactLinkContainer />
+      </div>
     </section>
   )
 }
