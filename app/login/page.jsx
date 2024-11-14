dbffe39da34c6cd3dd3705e2453235de2303bee1
solovyovsdom <joelborofsky@mac.com>
2024-11-13 22:44:28 -0600

    Refactor LoginPage layout; replace PageHeader with colored background divs and implement grid styling

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,10 +1,12 @@
-import PageHeader from '@/components/typography/headers/page-header/PageHeader'
 import styles from './page.module.scss'
 
 export default function LoginPage() {
     return (
         <section className={styles.loginPage}> 
-         <PageHeader>Log in!</PageHeader>
+         <div className={`bg-red ${styles.backgroundDiv} ${styles.backgroundDiv__one}`}></div>
+         <div className={`bg-blue ${styles.backgroundDiv} ${styles.backgroundDiv__two}`}></div>
+         <div className={`bg-green ${styles.backgroundDiv} ${styles.backgroundDiv__three}`}></div>
+         <div className={`bg-yellow ${styles.backgroundDiv} ${styles.backgroundDiv__four}`}></div>
          </section>
     )
 }
